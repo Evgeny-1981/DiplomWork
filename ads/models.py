52afe5035b7d7bb1a22df0f79f1ac50f92910e2c
Evgeny Egorov <EAEgorov42@gmail.com>
2024-12-04 14:19:04 +0500

    создал Dockerfile, docker-compose. Исправлены файлв .env

--- ads/models.py
+++ ads/models.py
@@ -8,19 +8,29 @@ NULLABLE = {"blank": True, "null": True}
 class Ad(models.Model):
     """Создание модели объявления"""
 
-    title = models.CharField(max_length=255, verbose_name='Название объявления')
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Стоимость товара/услуги')
-    description = models.TextField(default='Автор пока не создал описания', verbose_name='Описание', **NULLABLE)
-    author = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='owner',
-                               verbose_name='Владелец объявления', **NULLABLE)
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания объявления',
-                                      **NULLABLE)
+    title = models.CharField(max_length=255, verbose_name="Название объявления")
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name="Стоимость товара/услуги"
+    )
+    description = models.TextField(
+        default="Автор пока не создал описания", verbose_name="Описание", **NULLABLE
+    )
+    author = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        related_name="owner",
+        verbose_name="Владелец объявления",
+        **NULLABLE,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name="Дата создания объявления", **NULLABLE
+    )
 
     class Meta:
         db_table = "ads"
-        verbose_name = 'Объявление'
-        verbose_name_plural = 'Объявления'
-        ordering = ('-created_at',)
+        verbose_name = "Объявление"
+        verbose_name_plural = "Объявления"
+        ordering = ("-created_at",)
 
     def __str__(self):
-        return f'{self.title}, {self.created_at}, {self.author}, {self.price}, '
+        return f"{self.title}, {self.created_at}, {self.author}, {self.price}, "
