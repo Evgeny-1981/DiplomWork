87281209b1efe9793185775f080ce47158f40865
Evgeny Egorov <EAEgorov42@gmail.com>
2024-12-05 00:54:41 +0500

    делаем readme

--- users/tests.py
+++ users/tests.py
@@ -13,4 +13,9 @@ class TestRegisterUser(APITestCase):
             "password": "test",
         }
         response = self.client.post(url, data, format="json")
+        data = response.json()
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(data["email"], "test@test.ru")
+        self.assertEqual(data["role"], "user")
+        self.assertEqual(data["uid"], None)
+        self.assertEqual(data["token"], None)
